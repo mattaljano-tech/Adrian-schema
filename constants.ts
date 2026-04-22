e521a62caeaa58ec6e3a27ecda38d3a8fe9c0cc9
mattaljano-tech <mattaljano@gmail.com>
2026-04-22 16:04:58 +0200

    Add constants for configuration and magic numbers

---
+++ constants.ts
@@ -0,0 +1,8 @@
+// Constants for configuration and magic numbers
+export const TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+export const DEFAULT_SCHEDULE = [
+    { hour: 9, minute: 0 },
+    { hour: 12, minute: 0 },
+    { hour: 15, minute: 0 },
+];
+// Add more constants as needed
